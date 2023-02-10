389089025d9ea2da67c53e1cf306011a38f5b25b
Mina Abdelmalak <minaabdelmalak2016@gmail.com>
2023-02-10 08:07:07 +0200

    update seller forgot password blade

--- resources/views/seller/auth/forgot-password.blade.php
+++ resources/views/seller/auth/forgot-password.blade.php
@@ -1,30 +1,3 @@
-{{-- <x-guest-seller-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('sellers.password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-seller-layout> --}}
-
-
 @extends('seller.layouts.parent')
 
 @section('title', 'Forgot password')
@@ -37,8 +10,8 @@
 @parent
 <div class="container d-flex justify-content-center m-t-250">
     <div class="w-50 shadow p-3 mb-5 bg-body rounded">
-        <!-- Session Status -->
         @if (session('status'))
+            <!-- Session Status -->
             <div class = 'font-medium text-sm text-green-600 dark:text-green-400'>
                 {{ session('status') }}
             </div>
@@ -71,12 +44,9 @@
                         <button type="submit" class="btn btn-dark btn-block rounded-pill" style="background-color: black;">Email Password Reset
                             Link</button>
                     </div>
-                    <!-- /.col -->
                 </div>
             </form>
         </div>
-        <!-- /.form-box -->
-        <!-- /.card -->
     </div>
 </div>
 @endsection
